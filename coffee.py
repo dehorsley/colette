889954c23390894a571b77d1a664a841f7eb87c4
David Horsley <david.e.horsley@gmail.com>
2020-11-28 23:43:50 +1100

    add pair roles

--- coffee.py
+++ coffee.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import csv
 import math
+import random
 
 import mip
 
@@ -11,10 +12,11 @@ from functools import cache
 
 # NB: these must all be integers!
 COST_OF_NOT_PAIRING = 100  # Currently not used
-COST_OF_PARING_WITHIN_ORG = 1
-COST_OF_PARING_PREVIOUS_PARTNERS = 10
+COST_OF_PARING_WITHIN_ORG = 5
+COST_OF_PARING_PREVIOUS_PARTNERS = 100
+COST_OF_PARING_SAME_TYPE = 2  # as in role: orgraniser or coffee buyer
 
-# TODO: class people as "organisers" or "coffee buyers" and prefer to alternate
+random.seed(1987)  # for reproducability
 
 
 @dataclass(frozen=True)
@@ -23,34 +25,21 @@ class Person:
     organisation: str
 
 
+@dataclass(frozen=True)
+class Pair:
+    organiser: Person
+    buyer: Person
+
+    def __contains__(self, item):
+        return self.organiser == item or self.buyer == item
+
+
 Round = list[Person]
-Pairing = dict[Person, Person]
+Pairing = dict[Person, Pair]
 Cost = float
 
 
-def coffee_pairs(people: Round, previous_pairings: list[Pairing]) -> Pairing:
-    def weight_of_pair(p1: Person, p2: Person):
-        cost = 0
-        if p1.organisation == p2.organisation:
-            cost += COST_OF_PARING_WITHIN_ORG
-
-        for n, pairing in enumerate(previous_pairings):
-            if p1 not in pairing:
-                continue
-            if pairing[p1] is not p2:
-                continue
-
-            # TODO: this should take into account the last time this pair was *available*
-            # i.e. if someone goes in break, you don't want to pair them up with the same person,
-            # when they came back even if there was a large number of rounds between
-
-            if len(previous_pairings) - n == 1:
-                cost += 1_000_000
-            elif len(previous_pairings) - n < 20:
-                cost += COST_OF_PARING_PREVIOUS_PARTNERS
-        return cost
-
-    N = len(people)
+def coffee_pairs(N, weights) -> Pairing:
     pairs = [(i, j) for i in range(N - 1) for j in range(i + 1, N)]
 
     def pairs_containing(k):
@@ -64,9 +53,7 @@ def coffee_pairs(people: Round, previous_pairings: list[Pairing]) -> Pairing:
     for k in range(N):
         m += mip.xsum(p[i, j] for i, j in pairs_containing(k)) == 1
 
-    m.objective = mip.minimize(
-        mip.xsum(weight_of_pair(people[i], people[j]) * p[i, j] for i, j in pairs)
-    )
+    m.objective = mip.minimize(mip.xsum(weights(i, j) * p[i, j] for i, j in pairs))
 
     m.verbose = False
     status = m.optimize()
@@ -74,14 +61,7 @@ def coffee_pairs(people: Round, previous_pairings: list[Pairing]) -> Pairing:
         raise Exception("not optimal" + status)
     print("Objective value =", m.objective_value)
 
-    pairing = {}
-    for (i, j) in pairs:
-        if p[i, j].x < 0.5:
-            continue
-        pairing[people[j]] = people[i]
-        pairing[people[i]] = people[j]
-
-    return pairing
+    return [(i, j) for i, j in pairs if p[i, j].x > 0.5]
 
 
 def main():
@@ -109,23 +89,98 @@ def main():
     for path in round_paths:
         previous_pairing: Pairing = {}
         with path.open() as f:
-            for row in csv.reader(f):
+            for row in csv.DictReader(f):
                 # TODO: a more useful error might be nice here if lookup fails
-                p1, p2 = people_by_name[row[0]], people_by_name[row[1]]
-                previous_pairing[p1] = p2
-                previous_pairing[p2] = p1
+                p1 = people_by_name[row["organiser"]]
+                p2 = people_by_name[row["buyer"]]
+                pair = Pair(p1, p2)
+                previous_pairing[p1] = pair
+                previous_pairing[p2] = pair
             previous_pairings.append(previous_pairing)
 
-    pairings = coffee_pairs(round, previous_pairings)
+    def last_pairing(p: Person) -> Pair:
+        return next(
+            (pairings[p] for pairings in reversed(previous_pairings) if p in pairings),
+            None,
+        )
 
+    def make_pair(p1: Person, p2: Person) -> Pair:
+        """
+        makes a pair, assigning roles based on previous assignments
+        """
+
+        p1_last_pair = last_pairing(p1)
+        p2_last_pair = last_pairing(p2)
+
+        if (p1_last_pair is None or p1 == p1_last_pair.organiser) and (
+            p2_last_pair is None or p2 == p2_last_pair.buyer
+        ):
+            return Pair(organiser=p2, buyer=p1)
+
+        if (p1_last_pair is None or p1 == p1_last_pair.buyer) and (
+            p2_last_pair is None or p2 == p2_last_pair.organiser
+        ):
+            return Pair(organiser=p1, buyer=p2)
+
+        # Perhaps random
+        if random.random() < 0.5:
+            return Pair(organiser=p1, buyer=p2)
+
+        return Pair(organiser=p2, buyer=p1)
+
+    def weights(i, j):
+        p1 = round[i]
+        p2 = round[j]
+        cost = 0
+        if p1.organisation == p2.organisation:
+            cost += COST_OF_PARING_WITHIN_ORG
+
+        # if partners were of the same type last time
+
+        p1_last_pair = last_pairing(p1)
+        p2_last_pair = last_pairing(p2)
+
+        if p1_last_pair is not None and p2_last_pair is not None:
+            if (
+                p1 == p1_last_pair.organiser
+                and p2 == p2_last_pair.organiser
+                or p1 == p1_last_pair.buyer
+                and p2 == p2_last_pair.buyer
+            ):
+                cost += COST_OF_PARING_SAME_TYPE
+
+        # if partners were previously paired
+        for n, pairing in enumerate(previous_pairings):
+            if p1 not in pairing:
+                continue
+            if p2 not in pairing[p1]:
+                continue
+
+            # TODO: maybe this should take into account the last time this pair
+            # was *available* i.e. if someone goes in break, you don't want to
+            # pair them up with the same person, when they came back even if
+            # there was a large number of rounds between
+
+            if len(previous_pairings) - n == 1:
+                cost += 1_000_000
+            elif len(previous_pairings) - n < 20:
+                cost += COST_OF_PARING_PREVIOUS_PARTNERS
+        return cost
+
+    pairings = coffee_pairs(len(round), weights)
+    print(pairings)
+
+    # Round number to save
     try:
         N = int(round_paths[-1].stem.removeprefix("round_")) + 1
     except IndexError:
         N = 1
 
     with Path("test_data").joinpath(f"round_{N:03d}.csv").open("w") as f:
-        for p1, p2 in set(map(frozenset, pairings.items())):
-            print(p1.name, p2.name, sep=",", file=f)
+        print("organiser", "buyer", sep=",", file=f)
+        for i, j in pairings:
+            pair = make_pair(round[i], round[j])
+            print(pair.organiser.name, pair.buyer.name, sep=",", file=f)
 
 
 if __name__ == "__main__":
